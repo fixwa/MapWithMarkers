6a2acbbd787b1e701fae77b86950cb84137f8a2c
Pablo Nuñez <fixwah@gmail.com>
2017-07-26 19:26:06 -0300

    use volley as dependency

--- app/src/main/java/mwm/mapwithmarkers/MapsActivity.java
+++ app/src/main/java/mwm/mapwithmarkers/MapsActivity.java
@@ -3,6 +3,7 @@ package mwm.mapwithmarkers;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,6 +14,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
